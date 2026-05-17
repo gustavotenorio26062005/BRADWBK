97d5056e5fc6b580962f0c7b30bc9247abeffc15
Gustavo Barros Tenório <129299486+gustavotenorio26062005@users.noreply.github.com>
2026-05-17 10:44:07 -0300

    Create Categoria.java

---
+++ Aula 7/entity/Categoria.java
@@ -0,0 +1,26 @@
+package com.biblioteca.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+}
